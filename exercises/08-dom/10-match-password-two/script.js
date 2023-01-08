be69527200918463ec117f137a51f490bfe79e3d
ZinebElya <116351006+ZinebElya@users.noreply.github.com>
2023-01-08 17:42:42 +0100

    Update script.js

--- exercises/08-dom/10-match-password-two/script.js
+++ exercises/08-dom/10-match-password-two/script.js
@@ -11,17 +11,6 @@
 
 (() => {
     // your code 
-    
-    document.getElementById("run").addEventListener("click", () =>{
-
-        let pass1 = document.getElementById("pass-one");
-        let pass2 = document.getElementById("pass-two");
-        
-            if (pass1.value !== pass2.value) {
-                pass1.classList.add("error");
-                pass2.classList.add("error");
-            } 
-
-    })();
+  
    
 })();
