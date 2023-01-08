8df609321be046acf62675e3d84a08732372f2b2
ZinebElya <zineb.elyattouti@gmail.com>
2023-01-08 03:42:40 +0100

    dom 7 ok

--- exercises/08-dom/07-generate-table-one/script.js
+++ exercises/08-dom/07-generate-table-one/script.js
@@ -11,4 +11,29 @@
 
 (() => {
     // your code here
+
+    let table = document.createElement("table");
+    let tbody = document.createElement("tbody");
+    table.appendChild (tbody);
+
+    function monTableau(colonne, ligne) {
+
+        for (let i = 0; i < ligne; i++) {
+            tr = document.createElement("tr");
+            tbody.appendChild (tr);
+
+            for (let i = 0; i < colonne; i++) {
+                td = document.createElement("td");
+                tr.appendChild (td);
+                td.style.height = "30px";
+                
+            }
+        }
+    }
+
+    document.getElementById("target").appendChild(table);
+
+    monTableau(1,10); 
+    
+
 })();
